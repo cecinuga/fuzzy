71f0a8df7eed73c214c760c2b4668a489e1a28c1
cecinuga <cecinuga.tdb@gmail.com>
2025-10-04 01:00:07 +0200

    [#] refactored -parametrize-

--- fuzzy.go
+++ fuzzy.go
@@ -3,116 +3,65 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"crypto/tls"
-	"crypto/x509"
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
-
-	"software.sslmate.com/src/go-pkcs12"
 )
 
 var apiKey = ""
-var certPath = "./cert.pfx"
-var certPwd = "leonet1397"
 
 var methodFlag string
 var endpointFlag string
 var bodyFlag string
 var fieldsFlag string
 
-func Init() {
+func main() {
 	flag.StringVar(&methodFlag, "m", "GET", "[#] HTTP Request Method")
 	flag.StringVar(&endpointFlag, "e", "", "[#] Endpoint u wanna call")
 	flag.StringVar(&bodyFlag, "bp", "", "[#] HTTP Body request path")
 	flag.StringVar(&fieldsFlag, "fp", "", "[#] File with values to fuzz")
 
 	flag.Parse()
+	
+	fuzz()
 }
 
-func main() {
-	fmt.Fprintf(os.Stdout, helpManRoutine())
-
-	flag.StringVar(&methodFlag, "m", "GET", "[#] HTTP Request Method")
-	flag.StringVar(&endpointFlag, "e", "", "[#] Endpoint u wanna call")
-	flag.StringVar(&bodyFlag, "bp", "", "[#] HTTP Body request path")
-	flag.StringVar(&fieldsFlag, "fp", "", "[#] File with values to fuzz")
-
-	flag.Parse()
-
-	certData, err := os.ReadFile(certPath)
-	if err != nil {
-		log.Fatalf("Error reading PFX file: %v", err)
-	}
-	privateKey, certificate, caCerts, err := pkcs12.DecodeChain(certData, certPwd)
-	if err != nil {
-		log.Fatalf("Error decoding PFX data: %v", err)
-	}
-
-	certChain := [][]byte{certificate.Raw}
-	for _, caCert := range caCerts {
-		certChain = append(certChain, caCert.Raw)
-	}
-
-	cert := tls.Certificate{
-		Certificate: certChain,
-		PrivateKey:  privateKey,
-	}
-
-	rootCAs := x509.NewCertPool()
-	for _, caCert := range caCerts {
-		rootCAs.AddCert(caCert)
-	}
-
-	tlsConfig := &tls.Config{
-		Certificates:       []tls.Certificate{cert},
-		RootCAs:            rootCAs, // Optional: if you need to trust those CA certs
-		InsecureSkipVerify: true,    // Don't use unless testing!
-	}
-
-	client := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: tlsConfig,
-		},
-	}
+func fuzz(){
+	client := &http.Client{}
+	fieldValuesFilename := filepath.Base(fieldsFlag)
+	keyToValorize := strings.Replace(fieldValuesFilename, ".txt", "", 1)
 
 	switch methodFlag {
 	case http.MethodPost:
-		fuzzPost(client, endpointFlag, bodyFlag, fieldsFlag)
+		fuzzPost(client, fieldValuesFilename, keyToValorize)
 	case http.MethodGet:
-		fuzzGet(client, endpointFlag, fieldsFlag)
+		fuzzGet(client, fieldValuesFilename, keyToValorize)
 	}
 }
 
-func fuzzPost(client *http.Client, endpoint string, bodyPath string, fieldValuesPath string) {
-	fieldValuesFilename := filepath.Base(fieldValuesPath)
-	keyToValorize := strings.Replace(fieldValuesFilename, ".txt", "", 1)
-
-	bodyBuf, err := os.ReadFile(bodyPath)
+func fuzzPost(client *http.Client, fieldValuesFilename, keyToValorize string) {
+	bodyBuf, err := os.ReadFile(bodyFlag)
 	if err != nil {
 		log.Fatalf("Error reading bidy file: %v", err)
 	}
 	var bodyJson map[string]any
 	json.Unmarshal(bodyBuf, &bodyJson)
 
-	valuesFile, err := os.Open(fieldValuesPath)
+	valuesFile, err := os.Open(fieldsFlag)
 	if err != nil {
 		log.Fatalf("Error reading values file: %v", err)
 	}
 	defer valuesFile.Close()
 
-	found := false
-	foundedValue := ""
-
 	valuesScanner := bufio.NewScanner(valuesFile)
 	for valuesScanner.Scan() {
 		value := valuesScanner.Text()
+
 		bodyJson[keyToValorize] = value
 		bodyBuf, err := json.Marshal(bodyJson)
 		if err != nil {
@@ -120,7 +69,7 @@ func fuzzPost(client *http.Client, endpoint string, bodyPath string, fieldValues
 		}
 		bodyReader := bytes.NewReader(bodyBuf)
 
-		req, err := http.NewRequest(http.MethodPost, endpoint, bodyReader)
+		req, err := http.NewRequest(http.MethodPost, endpointFlag, bodyReader)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -135,46 +84,33 @@ func fuzzPost(client *http.Client, endpoint string, bodyPath string, fieldValues
 		}
 		defer res.Body.Close()
 
-		resBody, err := io.ReadAll(res.Body)
+		/*resBody, err := io.ReadAll(res.Body)
 		if err != nil {
 			log.Fatalf("Reading response body failed: %v", err)
-		}
-
-		if res.StatusCode != http.StatusNotFound {
-			found = true
-			foundedValue = value
-		}
+		}*/
 
 		fmt.Printf("[+] Request body {... '%v':%v ... }\n", keyToValorize, value)
-		fmt.Printf("[+] Response status: %s\n", res.Status)
-		fmt.Printf("%v\n\n\n", string(resBody))
+		fmt.Printf("[+] Response status: %s\n\n", res.Status)
+		//fmt.Printf("%v\n\n\n", string(resBody))
 	}
 	if err := valuesScanner.Err(); err != nil {
 		log.Fatalf("Error scanning value file: %v", err)
 	}
-
-	fmt.Printf("[#] Something Found: %v\n", found)
-	if found {
-		fmt.Printf("[+]\t%v: %v", keyToValorize, foundedValue)
-	}
 }
 
-func fuzzGet(client *http.Client, endpoint string, fieldValuesPath string) {
-	valuesFile, err := os.Open(fieldValuesPath)
+func fuzzGet(client *http.Client, fieldValuesFilename, keyToValorize string) {
+	valuesFile, err := os.Open(fieldsFlag)
 	if err != nil {
 		log.Fatalf("Error reading values file: %v", err)
 	}
 	defer valuesFile.Close()
-
-	found := false
-	foundedValue := ""
 
 	valuesScanner := bufio.NewScanner(valuesFile)
 	for valuesScanner.Scan() {
 		value := valuesScanner.Text()
 
 		bodyReader := bytes.NewReader(nil)
-		endpointWithParam := fmt.Sprintf("%v%v", endpoint, value)
+		endpointWithParam := fmt.Sprintf("%v%v", endpointFlag, value)
 		req, err := http.NewRequest(http.MethodGet, endpointWithParam, bodyReader)
 		if err != nil {
 			log.Fatal(err)
@@ -188,40 +124,16 @@ func fuzzGet(client *http.Client, endpoint string, fieldValuesPath string) {
 		}
 		defer res.Body.Close()
 
-		resBody, err := io.ReadAll(res.Body)
+		/*resBody, err := io.ReadAll(res.Body)
 		if err != nil {
 			log.Fatalf("Reading response body failed: %v", err)
-		}
-
-		if res.StatusCode != http.StatusNotFound {
-			found = true
-			foundedValue = value
-		}
+		}*/
 
 		fmt.Printf("[+] Endpoint: %v\n", endpointWithParam)
-		fmt.Printf("[+] Response status: %s\n", res.Status)
-		fmt.Printf("%v\n\n", string(resBody))
+		fmt.Printf("[+] Response status: %s\n\n", res.Status)
+		//fmt.Printf("%v\n\n", string(resBody))
 	}
 	if err := valuesScanner.Err(); err != nil {
 		log.Fatalf("Error scanning value file: %v", err)
 	}
-
-	fmt.Printf("[#] Something Found: %v\n", found)
-	if found {
-		fmt.Printf("[+]\t%v%v", endpoint, foundedValue)
-	}
-}
-
-func helpManRoutine() string {
-	helpMan := "[#] -m\tHTTP Request Method\n[#] -e\tEndpoint u wanna call\n[#] -bp\tHTTP Body request path\n[#] -fp\tFile with values to fuzz\n\nUsage: fuzzy.go -m GET -e <endpoint> file.txt"
-
-	if len(os.Args) == 1 {
-		return helpMan
-	}
-	for _, flag := range os.Args {
-		if flag == "--help" {
-			return helpMan
-		}
-	}
-	return ""
 }
