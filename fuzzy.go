552949ec33a1905d25f62b1137840e5f2763a969
cecinuga <cecinuga.tdb@gmail.com>
2025-10-05 03:50:49 +0200

    modularised code, better naming of variable

--- fuzzy.go
+++ fuzzy.go
@@ -19,22 +19,31 @@ var apiKey = ""
 var methodFlag string
 var endpointFlag string
 var bodyFlag string
-var fieldsFlag string
+var dictionaryFlag string
 var insecureFlag bool
 
 func main() {
 	flag.StringVar(&endpointFlag, "e", "", "[#] Endpoint u wanna call")
 	flag.StringVar(&methodFlag, "m", "GET", "[#] HTTP Request Method")
 	flag.StringVar(&bodyFlag, "bp", "", "[#] HTTP Body request path")
-	flag.StringVar(&fieldsFlag, "fp", "", "[#] File with values to fuzz")
+	flag.StringVar(&dictionaryFlag, "dict", "", "[#] Dictionary file")
 	flag.BoolVar(&insecureFlag, "k", false, "[#] Skip TLS certificate verification (insecure)")
 
 	flag.Parse()
 
-	fuzz()
+	transport := &http.Transport{}
+	if insecureFlag {
+		transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	}
+	
+	client := &http.Client{
+		Transport: transport,
+	}
+
+	fuzz(client)
 }
 
-func parseBody(key string, value string) *bytes.Reader {
+func parseBody(key, value string) *bytes.Reader {
 	bodyJson := make(map[string]any)
 	bodyJson[key] = value
 
@@ -47,39 +56,40 @@ func parseBody(key string, value string) *bytes.Reader {
 	return bodyReader
 }
 
-func fuzz() {
-	transport := &http.Transport{}
-	if insecureFlag {
-		transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-	}
-	
-	client := &http.Client{
-		Transport: transport,
-	}
+func getDictionaryFile() (name string, file *os.File) {
+	name = filepath.Base(dictionaryFlag)
 
-	fieldValuesFilename := filepath.Base(fieldsFlag)
-	keyToValorize := strings.Replace(fieldValuesFilename, ".txt", "", 1)
-
-	valuesFile, err := os.Open(fieldsFlag)
+	file, err := os.Open(dictionaryFlag)
 	if err != nil {
 		log.Fatalf("Error reading values file: %v", err)
 	}
-	defer valuesFile.Close()
 
-	valuesScanner := bufio.NewScanner(valuesFile)
+	return name, file
+}
+
+func buildRequest(fuzzKey, fuzzValue string) *http.Request {
+	body := parseBody(fuzzKey, fuzzValue)
+
+	req, err := http.NewRequest(methodFlag, endpointFlag, body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Method = methodFlag
+	req.Header.Add("Content-Type", "application/json")
+
+	return req
+}
+
+func fuzz(client *http.Client) {
+	dictName, dictFile := getDictionaryFile()
+	fuzzKey:= strings.Replace(dictName, ".txt", "", 1)
+	defer dictFile.Close()
+
+	valuesScanner := bufio.NewScanner(dictFile)
 	for valuesScanner.Scan() {
-		value := valuesScanner.Text()
+		fuzzValue := valuesScanner.Text()
 
-		bodyReader := parseBody(keyToValorize, value)
-
-		req, err := http.NewRequest(http.MethodPost, endpointFlag, bodyReader)
-		if err != nil {
-			log.Fatal(err)
-		}
-		req.Method = http.MethodPost
-		req.Header.Add("Content-Type", "application/json")
-		req.Header.Add("X-API-KEY", apiKey)
-
+		req := buildRequest(fuzzKey, fuzzValue)
 		res, err := client.Do(req)
 
 		if err != nil {
@@ -87,14 +97,8 @@ func fuzz() {
 		}
 		defer res.Body.Close()
 
-		/*resBody, err := io.ReadAll(res.Body)
-		if err != nil {
-			log.Fatalf("Reading response body failed: %v", err)
-		}*/
-
-		fmt.Printf("[+] Request body {... '%v':%v ... }\n", keyToValorize, value)
+		fmt.Printf("[+] Request body {... '%v':%v ... }\n", fuzzKey, fuzzValue)
 		fmt.Printf("[+] Response status: %s\n\n", res.Status)
-		//fmt.Printf("%v\n\n\n", string(resBody))
 	}
 	if err := valuesScanner.Err(); err != nil {
 		log.Fatalf("Error scanning value file: %v", err)
